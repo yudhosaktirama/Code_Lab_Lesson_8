e99c2489a89b24c3ee3f404b86373b3aabcf9d02
yudhosaktirama <yudhosakti89@gmail.com>
2023-09-08 20:12:37 +0700

    Inisiasi variabel currentScrambleWord secara terlambat

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,12 +6,13 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     private var _score = 0
     private  var _currentWordCount = 0
-    private var _currentScrambleWord = "test"
+    private lateinit var _currentScrambleWord : String
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
     init {
         Log.d("GameFragment", "Fragment Terbuat")
+        getNextWord()
     }
 
     override fun onCleared() {
