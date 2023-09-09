f52e4897c729a085d77e79772cdf1064d3a8c964
yudhosaktirama <yudhosakti89@gmail.com>
2023-09-10 06:42:02 +0700

    Melampirkan observer untuk currentwordcount

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -67,6 +67,11 @@ class GameFragment : Fragment() {
             { newScore ->
                 binding.score.text = getString(R.string.score, newScore)
             })
+        viewModel.currentWordCount.observe(viewLifecycleOwner,
+            { newWordCount ->
+                binding.wordCount.text =
+                    getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+            })
         viewModel.currentWord.observe(viewLifecycleOwner, {
             newword -> binding.textViewUnscrambledWord.text = newword
         })
