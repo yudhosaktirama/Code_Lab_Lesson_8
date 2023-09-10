789e0553ee1bcbd21ab088a2e3e36348a5dea439
yudhosaktirama <yudhosakti89@gmail.com>
2023-09-10 07:31:56 +0700

    Penambahan TalkBack

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.android.unscramble.ui.game
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
@@ -11,8 +15,21 @@ class GameViewModel : ViewModel() {
     val currentWordCount: LiveData<Int>
         get() = _currentWordCount
     private val _currentScrambleWord = MutableLiveData<String>()
-        val currentScrambledWord: LiveData<String>
-        get() = _currentScrambleWord
+    val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambleWord) {
+        if (it == null) {
+            SpannableString("")
+        } else {
+            val scrambledWord = it.toString()
+            val spannable: Spannable = SpannableString(scrambledWord)
+            spannable.setSpan(
+                TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                0,
+                scrambledWord.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable
+        }
+    }
     private var wordsList: MutableList<String> = mutableListOf()
     private  lateinit var currentWord: String
     val score: LiveData<Int>
